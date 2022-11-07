01b1d095d32f08b98756213abba6e7470cca18d4
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-11-07 10:12:20 +0100

    Exemples

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -1,2 +1,7 @@
-package basics;public class HelloWorld {
+package basics;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
 }
