70cfaca464b367417a2a5d81513665fd22fd2646
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-11-17 10:51:17 +0100

    Exemples

---
+++ src/bucles/ImprimirMultiples3.java
@@ -0,0 +1,14 @@
+package bucles;
+
+public class ImprimirMultiples3 {
+
+    public static void main(String[] args){
+
+        for(int n=33;n>=12;n--) {
+            if(n%3==0) {
+                System.out.printf("%d%n", n);
+            }
+        }
+
+    }
+}
