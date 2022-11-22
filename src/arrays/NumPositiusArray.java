d5d727a88fe259f70b0ce1590c2d1fc773448818
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-11-22 09:40:25 +0100

    Exemples

---
+++ src/arrays/NumPositiusArray.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class NumPositiusArray {
+
+    public static void main(String[] args){
+
+        int[] numeros = {3, -5, 2, 8, 9, -3, 0};
+        System.out.printf("L\'array té %d números positius.\n", numPositiusArray(numeros));
+
+    }
+
+    public static int numPositiusArray(int[] a){
+        int n = 0;
+        for(int i=0; i<a.length; i++){
+            if(a[i]>0){
+                n++;
+            }
+        }
+        return n;
+    }
+
+
+}
