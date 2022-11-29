90837539d0eafd007975624482928cba0f5e6b30
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-11-29 10:11:53 +0100

    Exemples

---
+++ src/recursius/CapicuaRecursiu.java
@@ -0,0 +1,22 @@
+package recursius;
+
+public class CapicuaRecursiu {
+
+    public static void main(String[] args){
+        System.out.printf("%s és capicua? %b.\n", "ahha", esCapicua("ahha"));
+        System.out.printf("%s és capicua? %b.\n", "ahxoyha", esCapicua("ahxoyha"));
+
+    }
+
+    public static boolean esCapicua(String s){
+        if(s.length()==1){
+            return true;
+        }
+        else if(s.length()==2){
+            return s.charAt(0)==s.charAt(1);
+        }
+        else {
+            return s.charAt(0)==s.charAt(s.length()-1) && esCapicua(s.substring(1, s.length()-1));
+        }
+    }
+}
---
+++ src/recursius/ParRecursiu.java
@@ -0,0 +1,20 @@
+package recursius;
+
+public class ParRecursiu {
+
+    public static void main(String[] args){
+
+        System.out.printf("%d és parell? %b.\n", 6, esParell(6));
+        System.out.printf("%d és parell? %b.\n", 11, esParell(11));
+
+    }
+
+    public static boolean  esParell(int n){
+        if(n==0){
+            return true;
+        }
+        else {
+            return !esParell(n-1);
+        }
+    }
+}
