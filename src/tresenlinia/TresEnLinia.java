1b216055d9261ac3ef1b26b3efceef14d021e66a
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-12-19 12:07:20 +0100

    Exemples

--- src/tresenlinia/TresEnLinia.java
+++ src/tresenlinia/TresEnLinia.java
@@ -1,9 +1,24 @@
 package tresenlinia;
+import java.util.Scanner;
 
 public class TresEnLinia {
+
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         initTauler();
         printTauler();
+        int[] t = demanaTirada(input);
+        tauler[t[0]][t[1]]=VALOR.X;
+        printTauler();
+    }
+
+    public static int[] demanaTirada(Scanner input){
+        int[] tirada = new int[2];
+        System.out.print("Fila: ");
+        tirada[0] = input.nextInt();
+        System.out.print("Columna: ");
+        tirada[1] = input.nextInt();
+        return tirada;
     }
 
     public enum VALOR {BUIDA, X, O};
@@ -20,12 +35,13 @@ public class TresEnLinia {
 
     public static void printTauler(){
         for(int f=0; f<tauler.length; f++){
+            System.out.printf("%d:\t", f);
             for(int c=0; c<tauler[0].length; c++) {
                 if(tauler[f][c]==VALOR.BUIDA) {
-                    System.out.printf(" \t");
+                    System.out.printf("_\t");
                 }
                 else {
-                    System.out.printf("%s \t", tauler[f][c]);
+                    System.out.printf("%s\t", tauler[f][c]);
                 }
             }
             System.out.println();
