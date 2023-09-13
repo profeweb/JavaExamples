3571d15ee5bcad0ccc90a03522873f5adf4361dc
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-09-13 13:59:28 +0200

    Exemples

--- src/marato/Main.java
+++ src/marato/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args){
 
+        // Variables de classe
         Runner r1, r2, r3, r4;
         Team t1, t2;
         Race c1, c2, c3;
+
+        // Construcció d'objectes de classes
 
         r1 = new Runner("Paco", "Playas", true);
         r2 = new Runner("Pep");
@@ -19,5 +22,20 @@ public class Main {
         c1 = new Race("Son Carrió");
         c2 = new Race("Puig Major");
         c3 = new Race("San Silvestre");
+
+        // Agregar Runners a Teams
+        t1.addRunner(r1);
+        t1.addRunner(r4);
+        t2.addRunner(r2);
+        t2.addRunner(r3);
+
+        // Agregar Runners a Races
+        c1.addRunner(r1);
+        c1.addRunner(r2);
+        c1.addRunner(r3);
+        c1.addRunner(r4);
+
+        c3.addRunner(r1);
+        c3.addRunner(r3);
     }
 }
