a0d878d4d2fd5b2a89f3a380ec8a8d29c741222e
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2023-11-09 08:38:50 +0100

    Exemples

--- src/basics/Divisio2Numeros.java
+++ src/basics/Divisio2Numeros.java
@@ -1,20 +1,14 @@
 package basics;
 
-import java.util.Scanner;
 
 public class Divisio2Numeros {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("A:");
-        int a = input.nextInt();
-
-        System.out.print("B:");
-        int b = input.nextInt();
-
+        int a = 24;
+        int b = 6;
         int d = a / b;
         System.out.println("DIVISIÓ de " + a + " entre " + b + " és " + d);
+        
     }
 }
---
+++ src/basics/Divisio2NumerosQualsevols.java
@@ -0,0 +1,20 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Divisio2NumerosQualsevols {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("A:");
+        int a = input.nextInt();
+
+        System.out.print("B:");
+        int b = input.nextInt();
+
+        int d = a / b;
+        System.out.println("DIVISIÓ de " + a + " entre " + b + " és " + d);
+    }
+}
