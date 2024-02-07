1a8145ba796d9fcee811e9e500776084fb178975
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-02-07 10:01:21 +0100

    Exemples

--- src/conversions/Binari2DecimalFuncio.java
+++ src/conversions/Binari2DecimalFuncio.java
@@ -1,15 +1,19 @@
 package conversions;
 import java.util.Scanner;
 
-public class Binari2Decimal {
+public class Binari2DecimalFuncio {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter Binary number: ");
-        int nb2 = input.nextInt();
+        int nb = input.nextInt();
+        int nd = binari2decimal(nb);
 
-        int nb = nb2;
+        System.out.printf("Binary number %d is decimal number %d.", nb, nd);
+    }
+
+    public static int binari2decimal(int nb){
         int nd = 0;
         int pot = 1;
 
@@ -19,6 +23,6 @@ public class Binari2Decimal {
             nd = r*pot + nd;
             pot = 2 * pot;
         }
-        System.out.printf("Binary number %d is decimal number %d.", nb2, nd);
+        return nd;
     }
 }
