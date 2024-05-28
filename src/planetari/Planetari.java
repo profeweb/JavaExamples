98948e331c24369d4bc8c2fc569c0d9e184adaf1
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-05-28 11:33:50 +0200

    Exemples

--- src/planetari/Planetari.java
+++ src/planetari/Planetari.java
@@ -1,6 +1,7 @@
 package planetari;
 
 import processing.core.PApplet;
+import processing.core.PShape;
 
 public class Planetari extends PApplet {
 
@@ -33,6 +34,7 @@ public class Planetari extends PApplet {
         // 1 Estrella
         cossos[0] = new Estrella ( "Sol", "2609", 'G', -26.8f );
         cossos[0].setImatge(this, "planetes/sun.svg");
+        cossos[0].setPosició(width/2, height/2, 0);
 
         // 2 Planetes
         cossos[1] = new Planeta( "Terra" , "2641" , Planeta.TipusPlaneta.ROCOS, (Estrella)cossos[0]);
@@ -61,12 +63,62 @@ public class Planetari extends PApplet {
     public void draw(){
         background(255, 100, 100);
 
+        displayInfo(50, 50, cossos[0]);
+
         cossos[0].display(this);
 
         // Dibuixa els botons
         bPlay.display(this); bPause.display(this);
         bPrev.display(this); bNext.display(this); bInfo.display(this);
         bZoomIn.display(this); bZoomOut.display(this);
+    }
+
+    void displayInfo(float x, float y, CosAstronomic c){
+
+        // Dibuixa el fons del panell informatiu
+        shapeMode(CORNER);
+        PShape panell = loadShape("panel.svg");
+        shape(panell, x, y);
+
+        // Dibuixa la imatge del planeta
+        shapeMode(CENTER);
+        shape(c.img, x + 60, y+55, 80, 80);
+
+        // Variables per definir el marge esquerre i l'altura entre línies.
+        float marginLeft = 80, lineHeight = 45;
+
+        // Font pel NOM del Cos Astronòmic
+        fill(20, 96, 88); textSize(40); textAlign(LEFT); //textFont(font1);
+
+        // Dibuixa el NOM del Cos Astronòmic
+        text(c.nom, x + 140, y + 80);
+
+        // Font per al TIPUS de Cos Astronòmic
+        fill(50); textSize(24); textAlign(LEFT); //textFont(font2);
+
+        // Dibuixa el TIPUS del cos astronòmic
+        text(c.tipus.toString(), x + 140, y + 120);
+
+        // Font per la resta de textos
+        fill(255,20,20); textSize(24); textAlign(LEFT); //textFont(font2);
+
+        // Dibuixa les dades del cos astronòmic (bloc 1)
+        text("dada 1", x + marginLeft, y + 210 +  lineHeight*0);
+        text("dada 2", x + marginLeft, y + 210 +  lineHeight*1);
+        text("dada 3", x + marginLeft, y + 210 +  lineHeight*2);
+        text("dada 4", x + marginLeft, y + 210 +  lineHeight*3);
+
+        // Dibuixa les dades del cos astronòmic (bloc 2)
+        text(String.valueOf(c.masa), x + marginLeft, y + 440 + lineHeight*0);
+        text("dada 2", x + marginLeft, y + 440 + lineHeight*1);
+        text("dada 3", x + marginLeft, y + 440 + lineHeight*2);
+        text("dada 4", x + marginLeft, y + 440 + lineHeight*3);
+
+        // Dibuixa les dades del cos astronòmic (bloc 3)
+        text(String.valueOf(c.radiOrbita), x + marginLeft, y + 655 + lineHeight*0);
+        text("dada 2", x + marginLeft, y + 655 + lineHeight*1);
+        text("dada 3", x + marginLeft, y + 655 + lineHeight*2);
+        text("dada 4", x + marginLeft, y + 655 + lineHeight*3);
     }
 
     public void mousePressed(){
