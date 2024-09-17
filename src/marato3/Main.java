c55bcc5b0e60e9a5fcd52d941da03de5e2c38828
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-09-17 10:22:41 +0200

    Exemples

--- src/marato3/Main.java
+++ src/marato3/Main.java
@@ -9,7 +9,11 @@ public class Main {
 
         // Constructor(s) de la classe Runner
         r1 = new Runner("Paco");
+        r1.setTeam("Equip A");
+        r1.setProfessional(true);
+
         r2 = new Runner("Bel", "Maravillas", true);
+        r2.setTeam("Equip B");
 
         // Mètodes de la classe Runner
         r1.addRace("Palma");
@@ -17,6 +21,9 @@ public class Main {
 
         r1.addRace("Inca");
         r1.addTime(12.24f);
+
+        r2.addRace("Palma");
+        r2.addTime(14.75f);
 
         System.out.printf("Millor carrera de %s va ser a %s amb temps %.2f. \n",
                 r1.getName(), r1.bestRace(), r1.bestTime());
@@ -31,7 +38,7 @@ public class Main {
 
         // Mètodes de la classe Team
         t1.addRunner(r1);
-        t1.addRunner(r2);
+        t2.addRunner(r2);
 
         System.out.printf("Temps mig de l'equip %s és %.2f. \n", t1.getName(), t1.averageTimes());
 
@@ -39,7 +46,7 @@ public class Main {
         Race c1, c2;
 
         // Constructor(s) de la classe Race
-        c1 = new Race("Carrera 1", false, 12);
+        c1 = new Race("Carrera 1", false, 13);
 
         // Mètodes de la classe Race
         c1.addRunner(r1);
@@ -48,25 +55,31 @@ public class Main {
         c1.addRunner(r2);
         c1.addTimeToRunner(r2, 14.3f);
 
-        System.out.printf("Millor corredor/a de la carrea %s és %s amb temps %.2f. \n", c1.getRaceID(), c1.bestRunner(), c1.bestTime());
+        System.out.printf("Millor corredor/a de la carrera %s és %s amb temps %.2f. \n", c1.getRaceID(), c1.bestRunner(), c1.bestTime());
 
+        // Mètodes Generals
+        System.out.printf("%s és el millor corredor/a de l'equip %s.\n", bestProfessionalRunnerOfTeam(t1), t1.getName());
+        printClassifiedRunners(c1, t1.getName());
+
+        Team[] teams = {t1, t2};
+        System.out.printf("%s és el millor equip.\n", bestTeam(teams));
 
     }
 
     public static String bestProfessionalRunnerOfTeam (Team t){
         float minTime = Float.MAX_VALUE;
-        String name = "";   // int index = -1;
+        String name = "";                           // int index = -1;
         for(int i=0; i<t.getNumRunners();  i++){
             Runner r = t.getRunners()[i];
             if(r.isProfessional()){
                 float time = r.bestTime();
                 if(time < minTime){
                     minTime = time;
-                    name = r.getName();  // index = i;
+                    name = r.getName();             // index = i;
                 }
             }
         }
-        return name;  // return t.getRunners[index].getName();
+        return name;                                // return t.getRunners[index].getName();
     }
 
     public static String bestTeam(Team[] teams){
@@ -83,11 +96,12 @@ public class Main {
     }
 
     public static void printClassifiedRunners(Race r, String nameTeam){
+        System.out.printf("Corredors classificats a la carrer %s: \n", r.getRaceID());
         for(int i=0; i<r.getNumRunners(); i++){
             Runner runner = r.getRunners()[i];
             float time = r.getTimes()[i];
             if(time < r.getClassificationTime() && runner.getTeam()== nameTeam){
-                System.out.println(runner.getName());
+                System.out.printf("\t%s amb temps %.2f.\n", runner.getName(), time);
             }
         }
     }
