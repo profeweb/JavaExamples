4c138f3d8a0c5a77bb0aeee79b7e173bbc32c5b2
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-10-25 08:21:08 +0200

    Exemples

--- src/basics/ImprimirEnumerats.java
+++ src/basics/ImprimirEnumerats.java
@@ -1,15 +1,27 @@
 package basics;
 
 class ImprimirEnumerats {
+
+    // Definició dels tipus enumerats
+     enum Sexe {HOME, DONA};
+     enum EstatCivil {SOLTER, CASAT, DIVORCIAT};
+     enum EstatLaboral {ESTUDIA, TREBALLA, ATUR, JUBILAT};
+     enum Estudis {CAP, ESO, BATXILLER, FP, UNIVERSITAT};
+
     public static void main(String[] args) {
 
-        // Definició dels valors del tipus enumerat DIA
-        enum DIA {DILLUNS, DIMARTS, DIMECRES, DIJOUS, DIVENDRES, DISSABTE, DIUMENGE };
+        // Variables de diferets tipus enumerats
+        Sexe a = Sexe.HOME;
+        Sexe b = Sexe.DONA;
+        EstatCivil c = EstatCivil.SOLTER;
+        EstatLaboral d = EstatLaboral.ATUR;
+        Estudis e = Estudis.FP;
 
-        // Variable del tipus DIA
-        DIA d = DIA.DIJOUS;
-        
-        // Imprimeix el valor de la variable d
+        // Impressió dels valors de les variable
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         System.out.println(d);
+        System.out.println(e);
     }
 }
