70cc41f8e8a0c3ac0fd872e15af1065c64d1a98c
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-12-03 20:01:26 +0100

    Exemples

--- src/arrays/NumerosPrimersArray.java
+++ src/arrays/NumerosPrimersArray.java
@@ -1,8 +1,45 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class NumerosPrimersArray {
 
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = input.nextInt();
+
+        int[] numeros = creaArrayPrimers(n);
+        printArray(numeros);
+
+    }
+
+    public static int[] creaArrayPrimers(int n){
+        int[] array = new int[n];
+
+        int numPrimers =  0;
+        int num = 2;
+        while(numPrimers<n){
+
+            int x = num - 1;
+            while(num % x !=0){
+                x = x -1;
+            }
+
+            if(x == 1){
+                array[numPrimers] = num;
+                numPrimers++;
+            }
+            num++;
+        }
+
+        return array;
+    }
+
+    public static void printArray(int[] array){
+        for(int i=0; i<array.length; i++){
+            System.out.printf("%d : %d.\n", i, array[i]);
+        }
     }
 }
