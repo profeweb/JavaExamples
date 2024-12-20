31425e6f13915b9d93551667d4ec3cd002ef70d6
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-12-20 08:28:38 +0100

    Exemples

--- src/arrays2d/EstadistiquesMatriu.java
+++ src/arrays2d/EstadistiquesMatriu.java
@@ -9,7 +9,11 @@ public class EstadistiquesMatriu {
                 {5, 6, 2, 0},
         };
 
+        System.out.printf("Estadístiques 1 (Min / Max / Avg): \n");
         printArray(estadistiques(dades));
+
+        System.out.printf("\nEstadístiques 2 (Min / Max / Avg): \n");
+        printArray(estadistiques2(dades));
     }
 
     public static void printArray(float[] array){
@@ -17,6 +21,9 @@ public class EstadistiquesMatriu {
             System.out.printf("%f\t", array[i]);
         }
     }
+
+
+    // Enfocament 1: funcions per separat (no eficient).
 
     public static float[] estadistiques(int[][] m){
         float[] e = new float[3];
@@ -59,5 +66,33 @@ public class EstadistiquesMatriu {
             }
         }
         return suma / num;
+    }
+
+    // Enfocament 2: una única funció (eficient).
+
+    public static float[] estadistiques2(int[][] m){
+
+        int minim = m[0][0];
+        int maxim = m[0][0];
+        float suma = 0;
+        int num = m.length * m[0].length;
+
+        for(int i=0; i<m.length; i++){
+            for(int j=0; j<m[i].length; j++){
+                if(m[i][j]<minim){
+                    minim = m[i][j];
+                }
+                if(m[i][j]>maxim){
+                    maxim = m[i][j];
+                }
+                suma += m[i][j];
+            }
+        }
+
+        float[] e = new float[3];
+        e[0] = minim;
+        e[1] = maxim;
+        e[2] = suma / num;
+        return e;
     }
 }
