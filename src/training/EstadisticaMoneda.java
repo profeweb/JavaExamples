4f4989684dd46305172d071ca03a7294d108a605
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-01-16 10:34:24 +0100

    Exemples

---
+++ src/training/EstadisticaMoneda.java
@@ -0,0 +1,34 @@
+package training;
+
+public class EstadisticaMoneda {
+
+    public static void main(String[] args) {
+
+        // Número de tirades
+        int numTirades = 1000000;
+
+        // Array per comptabilitzar les cares
+        float [] cares = new float[2];
+
+        // Fer les tirades i comptabilitzar els ocurrències
+        for(int i=0; i<numTirades; i++){
+            int d = tirarMoneda();
+            cares[d]++;
+        }
+
+        // Calcular les probabilitats
+        for(int i=0; i<cares.length; i++){
+            cares[i] /= numTirades;
+        }
+
+        // Mostrar resultats
+        System.out.printf("Probabilitat d'una cara és %f.%n", cares[0]);
+        System.out.printf("Probabilitat d'una creu és %f.%n", cares[1]);
+        System.out.printf("Total: %f.", cares[0]+cares[1]);
+
+    }
+
+    public static int tirarMoneda(){
+        return (int) Math.floor(Math.random()*2);
+    }
+}
