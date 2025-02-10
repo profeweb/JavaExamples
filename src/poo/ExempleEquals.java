4ba7d292f61e90e81a2403215747325d942be362
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-02-10 10:38:17 +0100

    Exemples

--- src/poo/ExempleEquals.java
+++ src/poo/ExempleEquals.java
@@ -4,9 +4,38 @@ public class ExempleEquals {
 
     public static void main(String[] args) {
 
+        Cotxe c1 = new Cotxe("PM12345CL");
+        Cotxe c2 = new Cotxe("PM12345CL");
+
+        System.out.println(c1);
+
+        if(c1.equals(c2)){
+            System.out.println("IGUALS");
+        }
+        else {
+            System.out.println("DIFERENTS");
+        }
     }
 
-    public class Cotxe {
+    public static class Cotxe {
+        String matricula;
+        int numPortes;
+        enum COLOR { VERMELL, VERD, BLAU};
+        COLOR color;
+
+        public Cotxe(String m){
+            this.matricula = m;
+            this.numPortes = 5;
+            this.color = COLOR.VERD;
+        }
+
+        // Setter i getters ...
+
+        public boolean equals(Object c){
+            Cotxe cc = (Cotxe) c;
+            return this.matricula.equals(cc.matricula) &&
+                    this.numPortes==cc.numPortes;
+        }
 
     }
 }
--- src/poo/ExempleToString.java
+++ src/poo/ExempleToString.java
@@ -4,10 +4,26 @@ public class ExempleToString {
 
     public static void main(String[] args) {
 
+        Persona p1 = new Persona("Paco", "45678761K", Persona.SEXE.HOME);
+        System.out.println(p1);
+
     }
 
-    public class Persona {
+    public static class Persona {
 
+        String nom, dni;
+        enum SEXE {HOME, DONA, ALTRES};
+        SEXE sexe;
+
+        public Persona(String n, String dni, SEXE s){
+            this.nom = n; this.dni = dni; this.sexe = s;
+        }
+
+        // Setters i getters ...
+
+        public String toString(){
+            return this.nom +", " + this.dni +" ("+this.sexe+")";
+        }
 
     }
 }
