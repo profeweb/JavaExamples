398a80d0eca96f35331a4b6c6f29f1cb4192d99b
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-02-12 09:55:59 +0100

    Exemples

--- src/poo/ExempleSignatura.java
+++ src/poo/ExempleSignatura.java
@@ -3,10 +3,12 @@ package poo;
 public class ExempleSignatura {
 
     public static void main(String[] args) {
-        int x = suma(4, 3, 5);
+        int x = calcul(4, 3, 5);
+        int y = calcul(2, 5);
     }
 
-    public static void suma(int x, int y){ }
+    public static int calcul(int a, int b){ return (a + b); }
 
-    public static int suma(int a, int b, int d){ return a + b; }
+    public static int calcul(int a, int b, int d){ return (a + b)*d; }
+
 }
