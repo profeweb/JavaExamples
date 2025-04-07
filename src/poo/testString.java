cca89210b454bd2a0cb68cb1a3a18f335c9d3ad8
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-04-07 12:51:44 +0200

    Exemples

--- src/poo/testString.java
+++ src/poo/testString.java
@@ -4,22 +4,26 @@ public class testString {
 
     public static void main(String[] args) {
 
-        // Objecte (Classe)
+        // Objecte String (Classe)
         String original = "Hola";
         original = metode(original);
-        System.out.print(original);
+        System.out.println(original);
 
-        // Tipus bàsic
+        // Tipus bàsic (int)
         int x = 10;
         metode(x);
-        System.out.print(x);
-        
+        System.out.println(x);
+
     }
 
+    // Mètode que rep objecte com a paràmetre i el modifica (passa a majúscules)
+    // Cal fer un mètode de retorn per a modificar el valor
     static String metode(String o){
         return o.toUpperCase();
     }
 
+    // Mètode que rep tipus bàsic com a paràmetre i el modifica (sumant 1)
+    // No cal fer un mètode de retorn per a modificar el valor
     static void metode(int x){
         x = x + 1;
     }
