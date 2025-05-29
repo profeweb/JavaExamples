f70117113ff3c627f05e15e6cb6b779a281f8df2
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-05-29 12:24:15 +0200

    Exemples

--- src/xarxes/CompressioText.java
+++ src/xarxes/CompressioText.java
@@ -3,7 +3,7 @@ package xarxes;
 public class CompressioText {
 
     public static void main(String[] args) {
-        String original = "ABCCCCCCC DEF GGGGGG ZUQ ";
+        String original = "ABCCCCCCC DEF GGGGGG ZUQ.";
         String zipped = zipText(original);
         System.out.println("Zipped Message: " + zipped);
         String unzipped = unzipText(zipped);
@@ -13,20 +13,26 @@ public class CompressioText {
     public static String zipText(String original){
         String  zipped = "";
         int count =  0, index = 0;
-        while(index < original.length()-1) {
+        while(index < original.length()) {
             char currentChar = original.charAt(index);
-            char nextChar = original.charAt(index + 1);
-            if (currentChar != nextChar) {
+            if(index== original.length()-1){
                 zipped += currentChar;
                 index++;
-            } else {
-                while (nextChar == currentChar) {
-                    count++;
-                    nextChar = original.charAt(index + count);
+            }
+            else {
+                char nextChar = original.charAt(index + 1);
+                if (currentChar != nextChar) {
+                    zipped += currentChar;
+                    index++;
+                } else {
+                    while (nextChar == currentChar) {
+                        count++;
+                        nextChar = original.charAt(index + count);
+                    }
+                    zipped += currentChar + String.valueOf(count);
+                    index += count;
+                    count = 0;
                 }
-                zipped += currentChar + String.valueOf(count);
-                index += count;
-                count = 0;
             }
         }
 
@@ -36,19 +42,24 @@ public class CompressioText {
     public static String unzipText(String zipped){
         String unzipped = "";
         int index = 0;
-        while(index<zipped.length()-1){
+        while(index<zipped.length()){
             char currentChar = zipped.charAt(index);
-            char nextChar = zipped.charAt(index + 1);
-            if(Character.isDigit(nextChar)){
-                int times = Character.getNumericValue(nextChar);
-                for(int t=0; t<times; t++){
-                    unzipped += currentChar;
-                }
-                index += 2;
-            }
-            else {
+            if(index == zipped.length()-1){
                 unzipped += currentChar;
                 index++;
+            }
+            else {
+                char nextChar = zipped.charAt(index + 1);
+                if (Character.isDigit(nextChar)) {
+                    int times = Character.getNumericValue(nextChar);
+                    for (int t = 0; t < times; t++) {
+                        unzipped += currentChar;
+                    }
+                    index += 2;
+                } else {
+                    unzipped += currentChar;
+                    index++;
+                }
             }
         }
         return unzipped;
