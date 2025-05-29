eb062ea6574e10b00d45ae793bcb758b49ce79c6
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-05-29 12:20:31 +0200

    Exemples

--- src/xarxes/CompressioText.java
+++ src/xarxes/CompressioText.java
@@ -3,9 +3,11 @@ package xarxes;
 public class CompressioText {
 
     public static void main(String[] args) {
-        String original = "ABCCCCCCC DEF GGGGGG ZUQ.";
+        String original = "ABCCCCCCC DEF GGGGGG ZUQ ";
         String zipped = zipText(original);
         System.out.println("Zipped Message: " + zipped);
+        String unzipped = unzipText(zipped);
+        System.out.println("Unzipped Message: " + unzipped);
     }
 
     public static String zipText(String original){
@@ -33,7 +35,22 @@ public class CompressioText {
 
     public static String unzipText(String zipped){
         String unzipped = "";
-        
+        int index = 0;
+        while(index<zipped.length()-1){
+            char currentChar = zipped.charAt(index);
+            char nextChar = zipped.charAt(index + 1);
+            if(Character.isDigit(nextChar)){
+                int times = Character.getNumericValue(nextChar);
+                for(int t=0; t<times; t++){
+                    unzipped += currentChar;
+                }
+                index += 2;
+            }
+            else {
+                unzipped += currentChar;
+                index++;
+            }
+        }
         return unzipped;
     }
 }
