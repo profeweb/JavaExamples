50dab1f5b60dc2026d044a7ac4b85527ffc70d90
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-05-30 11:57:10 +0200

    Exemples

--- src/xarxes/CodiCesar.java
+++ src/xarxes/CodiCesar.java
@@ -3,8 +3,7 @@ package xarxes;
 public class CodiCesar {
 
     public static final char[] ALPHABET26 = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-
+    
     public static char xifrar(char c, int shift){
 
         int pos = -1;
@@ -30,5 +29,13 @@ public class CodiCesar {
             return c;
         }
 
+    }
+
+    public static String xifrar(String original, int shift){
+        String xifrat="";
+        for(int i=0; i<original.length(); i++){
+            xifrat += xifrar(original.charAt(i), shift);
+        }
+        return xifrat;
     }
 }
