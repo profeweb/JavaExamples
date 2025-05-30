2e8eb5d0fe8d48ee80dea6918c9cc10dd8302cb6
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-05-30 08:58:33 +0200

    Exemples

--- src/xarxes/CodiCesar.java
+++ src/xarxes/CodiCesar.java
@@ -2,5 +2,5 @@ package xarxes;
 
 public class CodiCesar {
 
-    public static final char[] ALPHABET = {};
+    public static final char[] ALPHABET26 = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 }
