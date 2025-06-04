8d84952c06b4af28eacda997f5a37121601f771f
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-06-04 11:13:40 +0200

    Exemples

--- src/xarxes/CodiCesarASCII.java
+++ src/xarxes/CodiCesarASCII.java
@@ -18,6 +18,9 @@ public class CodiCesarASCII {
         char c2 = (char) asciiCode;
         System.out.printf("El caràcter del codi ASCII %d és %c.\n", asciiCode, c2);
 
+        // Detecció de Lletra de l'alfabet
+        System.out.printf("El caràcter %c és lletra: %b.\n", c2, Character.isLetter(c2));
+
         // Detecció de Majúscula
         System.out.printf("El caràcter %c és majúscula: %b.\n", c2, Character.isUpperCase(c2));
     }
