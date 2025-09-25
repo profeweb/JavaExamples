be6e3f7637e15e7c1884369b2f0d4a6205650a7a
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-09-25 09:51:26 +0200

    Exemples

--- src/swapping1/Memory.java
+++ src/swapping1/Memory.java
@@ -1,5 +1,7 @@
 package swapping1;
 
+import processing.core.PApplet;
+
 public class Memory {
 
     int mida;
@@ -12,6 +14,51 @@ public class Memory {
         procesos = new Proces[100];
         numProcesos = 0;
         freeAddress = 0;
+    }
+
+    void swapIn(Proces p){
+        if(freeAddress + p.mida < mida) {
+            procesos[numProcesos] = p;
+            numProcesos++;
+            p.setAddress(freeAddress);
+            freeAddress += p.mida;
+        }
+        else {
+            System.out.println("Error SWAP In del procés "+p.nom+"!");
+        }
+    }
+
+    void swapOut(Proces p){
+        for(int i=0; i<numProcesos; i++){
+            if(procesos[i]==p){
+                procesos[i] = null;
+                numProcesos--;
+                System.out.println("SWAPPED OUT "+ p.nom);
+            }
+        }
+    }
+
+
+    void display(PApplet p5){
+
+        p5.stroke(0); p5.strokeWeight(3); p5.fill(220);
+        p5.rect(100, 100, mida, 300);
+
+        p5.fill(0); p5.textSize(20); p5.textAlign(p5.LEFT);
+        p5.text("Memòria RAM", 100, 60);
+        p5.text("Total: "+mida, 100, 400);
+
+
+        for(int i=0; i<procesos.length; i++){
+            if(procesos[i]!=null) {
+                procesos[i].display(p5, 100, 100, 300);
+            }
+        }
+
+    }
+
+    int calculaMemoriaOcupada(){
+        return 0;
     }
 
 
--- src/swapping1/Swapping.java
+++ src/swapping1/Swapping.java
@@ -4,7 +4,8 @@ import processing.core.PApplet;
 
 public class Swapping extends PApplet {
 
-    Proces p1;
+    Proces p0, p1, p2, p3, p4;
+    Memory memory;
 
     public static void main(String[] args) {
         PApplet.main("swapping1.Swapping");
@@ -15,11 +16,38 @@ public class Swapping extends PApplet {
     }
 
     public void setup(){
+
+        memory = new Memory(1024);
+
+        p0 = new Proces("SO", color(0, 0, 255), 512);
         p1 = new Proces("P1", color(255, 0, 0), 64);
+        p2 = new Proces("P2", color(255, 255, 0), 128);
+        p3 = new Proces("P3", color(255, 0, 255), 256);
+        p4 = new Proces("P4", color(0, 255, 255), 128);
+
+        memory.swapIn(p0);
+
     }
 
     public void draw(){
         background(255);
-        p1.display(this, 100, 100, 50);
+        memory.display(this);
+
+        if(frameCount == 100){
+            memory.swapIn(p1);
+        }
+        else if(frameCount == 200){
+            memory.swapIn(p2);
+        }
+        else if(frameCount == 300){
+            memory.swapIn(p3);
+        }
+        else if(frameCount == 400){
+            memory.swapOut(p1);
+        }
+        else if(frameCount == 500){
+            memory.swapIn(p4);
+        }
+
     }
 }
