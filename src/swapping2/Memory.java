272b5a3df32307f685f7a19699a46fc4035911c3
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-09-29 10:48:20 +0200

    Exemples

--- src/swapping2/Memory.java
+++ src/swapping2/Memory.java
@@ -9,30 +9,52 @@ public class Memory {
     int numProcesos;
     int freeAddress;
 
+    Hole[] holes;
+    int numHoles;
+
     Memory(int m){
         this.mida = m;
         procesos = new Proces[100];
         numProcesos = 0;
         freeAddress = 0;
+
+        numHoles = 0;
+        holes = new Hole[100];
+        addHole(0, m);
+    }
+
+    void addHole(int a, int m){
+        holes[numHoles] = new Hole(a, m);
+        numHoles++;
+        System.out.println("New Hole");
     }
 
     void swapIn(Proces p){
-        if(freeAddress + p.mida < mida) {
-            procesos[numProcesos] = p;
-            numProcesos++;
-            p.setAddress(freeAddress);
-            freeAddress += p.mida;
-        }
-        else {
-            System.out.println("Error SWAP In del procés "+p.nom+"!");
+        boolean swapped = false;
+        for(int i=0; i<numHoles; i++){
+            if(holes[i]!=null && holes[i].mida >= p.mida){
+                int espaiRestant = holes[i].mida - p.mida;
+                procesos[numProcesos] = p;
+                procesos[numProcesos].setAddress(holes[i].address);
+                numProcesos++;
+                swapped = true;
+                System.out.println("SWAPPED IN "+ p.nom);
+
+                holes[i].address = holes[i].address + p.mida;
+                holes[i].mida = espaiRestant;
+                if(holes[i].mida ==0){
+                    holes[i] = null;
+                    System.out.println("Hole Removed");
+                }
+            }
         }
     }
 
     void swapOut(Proces p){
         for(int i=0; i<numProcesos; i++){
             if(procesos[i]==p){
+                addHole(p.address, p.mida);
                 procesos[i] = null;
-                numProcesos--;
                 System.out.println("SWAPPED OUT "+ p.nom);
             }
         }
