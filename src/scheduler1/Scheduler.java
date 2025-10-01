21f3473c486a6224c32a067d94454f3498a05421
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2025-10-01 09:48:02 +0200

    Exemples

--- src/scheduler1/Scheduler.java
+++ src/scheduler1/Scheduler.java
@@ -4,13 +4,18 @@ import processing.core.PApplet;
 
 public class Scheduler {
 
+    // Política del planificador
     enum POLITICA {FIFO, SHORT_JOB_FIRST, SHORT_REMAINING_JOB_FIRST, ROUND_ROBIN};
     POLITICA politica;
 
+    // Procesos a executar
     Proces[] procesos;
     int numProcesos;
 
+    // Número de cicles executats
     int numCicles = 0;
+
+    // Historial del planificador
     String log;
 
     public Scheduler(){
@@ -65,22 +70,24 @@ public class Scheduler {
         numCicles++;
     }
 
-
-
     public void display(PApplet p5, float x, float y, float s){
         p5.pushStyle();
 
+        // Política del planificador
         p5.fill(0); p5.textSize(24); p5.textAlign(p5.LEFT);
         p5.text("Scheduler " + this.politica, x, y);
 
+        // Estadístiques del planificador
         p5.textSize(18);
         p5.text("Num Procesos: "+ numProcesos, x, y + 40);
         p5.text("Num Cicles: "+ numCicles, x, y + 60);
 
+        // Dibuixa els processos de la cua del planificador
         for(int i=0; i<numProcesos; i++){
             procesos[i].display(p5, x + 200 + i*(s+5), y + 40, s, s);
         }
 
+        // Historial d'execucions de procesos
         p5.fill(0);
         p5.text(log, x, y + 140);
         p5.popStyle();
