fd499190fc568d4fd7819083030af1abf2bc71ee
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2026-02-24 08:48:50 +0100

    Exemples

--- src/casting/ArraySuperClasse.java
+++ src/casting/ArraySuperClasse.java
@@ -2,20 +2,50 @@ package casting;
 
 public class ArraySuperClasse {
 
+    public static void main(String[] args) {
+        Persona[] persones = new Persona[3];
+        persones[0] = new Alumne("Pep", 4);
+        persones[1] = new Professor("Jaume", "Mates");
+        persones[2] = new Alumne("Bel", 3);
+
+        // Imprimeix amb el mètode print de cada subclasse
+        for(int i=0; i<persones.length; i++){
+            persones[i].print();
+        }
+
+        // Imprimeix només els objectes d'una subclasse
+        for(int i=0; i<persones.length; i++){
+            if(persones[i] instanceof Alumne) {
+                persones[i].print();
+            }
+        }
+
+        // Comptabilitzar els objectes de cada subclasse
+        int numAlumnes = 0, numProfes = 0;
+        for(int i=0; i<persones.length; i++){
+            if(persones[i] instanceof Alumne) {
+                numAlumnes++;
+            }
+            else if(persones[i] instanceof Professor) {
+                numProfes++;
+            }
+        }
+        System.out.printf("Num. Alumnes: %d, Num. Profes: %d.\n", numAlumnes, numProfes);
+    }
 
     static class Persona {
         String nom;
         Persona(String nom){ this.nom = nom; }
-        void print(){ System.out.print("Persona: "+ this.nom); }
+        void print(){ System.out.println("Persona: "+ this.nom); }
     }
-    static class Alumne extends ExempleCasting.Persona {
+    static class Alumne extends Persona {
         int curs;
         Alumne(String nom, int curs){ super(nom); this.curs = curs; }
-        void print(){ System.out.print("Alumne: "+ this.nom+", "+this.curs); }
+        void print(){ System.out.println("Alumne: "+ this.nom+", "+this.curs); }
     }
-    static class Professor extends ExempleCasting.Persona {
+    static class Professor extends Persona {
         String departament;
         Professor(String nom, String dep){ super(nom); this.departament = dep; }
-        void print(){ System.out.print("Profe: "+ this.nom+", "+this.departament); }
+        void print(){ System.out.println("Profe: "+ this.nom+", "+this.departament); }
     }
 }
